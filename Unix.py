d24fb31cdee5b548ca9c3cab012bb1b821a1cbcc
Marcus Willock <crazcalm@gmail.com>
2014-05-24 18:46:16 -0400

    creating the package

---
+++ Unix.py
@@ -0,0 +1,40 @@
+"""
+                Unix
+                ----
+
+Introduction:
+-------------
+
+  I an currently at a 24 hour hackathons (with 16 hours left) and I would like
+to create a Unix class that allows me to use my typical Unix commands on my
+Windows machines (Laptop, and old XP). 
+
+  I originally was going to create a wrapper for the Windows termanal, but
+then I remembered that the list of windows commands for my laptop and XP
+are not the same.
+
+  So, I am goint to write this class using only the python standard library.
+Wish me luck!
+
+
+Current functionality:
+----------------------
+
+
+Wanted functionality:
+---------------------
+
+ls
+cd
+mkdir
+"""
+
+class Unix(object):
+    """
+    Porting a number of the unix commands to Windows
+    """
+    def __init__(self):
+        print "You now have access to the a few linux commands!"
+
+
+
