8bbadec4a5ab64644ab1318f11a374099264a182
Marcus Willock <crazcalm@gmail.com>
2014-05-24 18:58:16 -0400

    Creating another needed file

---
+++ Terminal.py
@@ -0,0 +1,16 @@
+"""
+                    Terminal:
+                    ---------
+
+Goal:
+----
+
+  I would like to write a script to create a artificial termial window.
+
+Plan:
+-----
+
+  I will create a looping input that closes when the "exit" is typed.
+"""
+
+import sys
